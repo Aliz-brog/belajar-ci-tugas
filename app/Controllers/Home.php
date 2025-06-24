018409934f57773c02aa6ce9d166a2e4bebde355
Aliz-brog <cez46520@gmail.com>
2025-06-24 19:14:54 +0700

    menambahkan api data transaksi pembelian

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,17 +2,23 @@
 
 namespace App\Controllers;
 
-use App\Models\ProductModel; 
+use App\Models\ProductModel;
+use App\Models\TransactionModel;
+use App\Models\TransactionDetailModel;
 
 class Home extends BaseController
 {
     protected $product;
+    protected $transaction;
+    protected $transaction_detail;
 
     function __construct()
     {
         helper('number');
         helper('form');
         $this->product = new ProductModel();
+        $this->transaction = new TransactionModel();
+        $this->transaction_detail = new TransactionDetailModel();
     }
 
     public function index()
